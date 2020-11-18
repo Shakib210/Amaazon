c71cb7655322b7494c6562db6397d28d67daeb07
Shakib210 <nshakib70@gmail.com>
2020-11-18 20:41:27 +0600

    first commit

---
+++ Frontend/build/precache-manifest.709b6de572a25001742cfc3545b6c7a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4323a7cd9a01411741aaee001cce316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5688461b5d01b04a55",
+    "url": "/static/css/main.62b7980e.chunk.css"
+  },
+  {
+    "revision": "4e48db3323b21bfb720a",
+    "url": "/static/js/2.854b7d9b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.854b7d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5688461b5d01b04a55",
+    "url": "/static/js/main.eebde64c.chunk.js"
+  },
+  {
+    "revision": "338f2d16123db1bb237b",
+    "url": "/static/js/runtime-main.f1c5e8c8.js"
+  }
+]);
